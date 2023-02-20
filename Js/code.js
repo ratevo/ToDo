5f3e1453742bdfa7ffb94440752bd3ce19b59c6b
steven <steven.immune@gmail.com>
2023-02-20 11:56:29 -0800

    add js code result

--- Js/code.js
+++ Js/code.js
@@ -1,1 +1,15 @@
 console.log('Hallo Java Script')
+
+
+const todoInput = document.querySelector (".task-input")
+const todoBtn = document.querySelector (".task-btn")
+
+function addTodo(e){
+    e.preventDefault();
+    console.log(todoInput.value)
+    todoBtn.value = ''
+}
+
+todoBtn.addEventListener('click',addTodo)
+
+// Code Search
